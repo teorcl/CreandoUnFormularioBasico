8e1787a9dfad18adb418fbd71acc4441e19dc078
Teodoro <teo.calle.lara@gmail.com>
2022-10-24 14:39:26 -0500

    Initial Commit

---
+++ CreandoUnFormularioBasico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreandoUnFormularioBasico
+//
+//  Created by TEO on 24/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
